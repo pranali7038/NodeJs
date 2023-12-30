afd31b1b52b375c33432d06869b064ee4f569101
pranali7038 <143324397+pranali7038@users.noreply.github.com>
2023-12-30 22:23:37 +0530

    Add files via upload

---
+++ ModWrapperFun/index.js
@@ -0,0 +1,8 @@
+(function(exports,require,module,__filename,__dirname){
+    const name='pranali jahagirdar';
+    console.log(name);
+})();
+//console.log(name); not able to used here because data is private due to module wrapper funtion
+console.log(__dirname);
+console.log(__filename);
+
