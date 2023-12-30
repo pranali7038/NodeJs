da14a8724c65ea53f0ddc8b399de32749581144a
pranali7038 <143324397+pranali7038@users.noreply.github.com>
2023-12-30 22:19:20 +0530

    Add files via upload

---
+++ importExport/index.js
@@ -0,0 +1,6 @@
+const {add,sub}=require('./operator');
+//console.log(operator);
+//console.log(operator.add(5,5));
+//console.log(operator.sub(15,5));
+console.log(add(10,10));
+console.log(sub(40,21));
